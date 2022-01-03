4a9372299f522c16d10c07d43991361e9fcd82f4
Joseph-GM <jkim.english75@gmail.com>
2022-01-03 12:53:34 +0900

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Joseph Kim on 2022/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
